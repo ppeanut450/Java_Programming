461d0386ad5c4d99dd63ec9b1010923285dd7d5d
ppeanut450 <ppeanut450@gmail.com>
2020-10-07 11:47:28 +0900

    week 3(2)

---
+++ 3주 실습/src/NestedLoop.java
@@ -0,0 +1,15 @@
+public class NestedLoop 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<10; i++)									// 1단에서 9단
+		{
+			for(int j=1; j<10; j++)								// 각 단의 구구단 출력
+			{
+				System.out.print(i + "*" + j + "=" + i*j);		// 구구단 출력
+				System.out.print('\t');							// 하나씩 탭으로 띄기
+			}
+			System.out.println();
+		}
+	}
+}
